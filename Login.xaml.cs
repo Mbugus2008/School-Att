45f527ed6fa8dfd3ccdb4ad21c0819f633787b1e
Paul Njoroge <mbugus2008@gmail.com>
2019-05-27 13:20:45 +0300

    Add project files.

---
+++ Login.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace School
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        SchoolEntities db = new SchoolEntities();
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void login(object sender, RoutedEventArgs e)
+        {
+            var user = db.Users.FirstOrDefault(o => o.Username == usernameTextBox.Text && o.Password == passwordTextBox.Text);
+            if (user == null)
+            {
+                MessageBox.Show("Username or Password is invalid");
+            }
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+       
+        }
+    }
+}
